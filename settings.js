471c880eb368a5a9ce94ca75b96c8b0e3f17b1f1
MrHackerLordz <165281290+MrHackerLordz@users.noreply.github.com>
2024-08-26 12:35:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "༒MrHãçI{ërLørd༒" //ur yt chanel name
+global.socialm = "GitHub: MrHackerLordz" //ur github or insta name
+global.location = "precised Location" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.botname = '༒MrHãçI{ërLørd Botz༒' //ur bot name
+global.ownernumber = '2348184059232' //ur owner number
+global.ownername = '༒MrHãçI{ërLørd༒' //ur owner name
+global.websitex = "https://youtu.be/@humblesmart951"
+global.wagc = "https://chat.whatsapp.com/Br36qv7KCGK7xQyw252jNA"
+global.themeemoji = '🧑‍💻'
+global.wm = "Lordz Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "༒MrHãçI{ërLørd༒\n\n+916909137213"
+global.creator = "2348184059232@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["2348184059232"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -39,10 +39,10 @@ global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
+global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = false //auto status/story view
-global.adminevent = false //show promote/demote message
+global.adminevent = true //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
 global.mess = {
@@ -53,7 +53,7 @@ global.mess = {
     success: 'Here you go!'
 }
 //thumbnail
-global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic0.jpg')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
